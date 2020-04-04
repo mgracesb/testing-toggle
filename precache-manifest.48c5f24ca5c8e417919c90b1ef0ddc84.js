c09c01b2d112ffcbc4ed25d30e83af8ed776d163
mgracesb <mercysbarcel@gmail.com>
2020-04-04 20:20:19 +0200

    Updates

---
+++ precache-manifest.48c5f24ca5c8e417919c90b1ef0ddc84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102f9268587537daf245dbb882ba2f6d",
+    "url": "/testing-toggle/index.html"
+  },
+  {
+    "revision": "2c82a3aa985496787c91",
+    "url": "/testing-toggle/static/css/main.2905a3a3.chunk.css"
+  },
+  {
+    "revision": "8f82e6da9f9393413253",
+    "url": "/testing-toggle/static/js/2.f4acfc29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testing-toggle/static/js/2.f4acfc29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c82a3aa985496787c91",
+    "url": "/testing-toggle/static/js/main.9574fe45.chunk.js"
+  },
+  {
+    "revision": "7fb41086a1a6c5f43b8c",
+    "url": "/testing-toggle/static/js/runtime-main.35b87cbb.js"
+  },
+  {
+    "revision": "435e99ab2f74ecc8ea45ccce54f602f4",
+    "url": "/testing-toggle/static/media/moon.435e99ab.svg"
+  },
+  {
+    "revision": "bbab351f138bac6a6e70c8783a68e30d",
+    "url": "/testing-toggle/static/media/sun.bbab351f.svg"
+  }
+]);
